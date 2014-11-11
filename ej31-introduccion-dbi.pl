3fec37af4924d6beafd9609c8305dc38ff95f756
Juan Paredes <jparedes@mppee.gob.ve>
2014-11-11 22:00:56 +0100

    Ejemplo de introducción a DBI

---
+++ ej31-introduccion-dbi.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8::all;
+use v5.10;
+
+use Data::Dumper;
+
+# usamos el módulo DBI y drivers requeridos
+use DBI;
+use DBD::Pg;
+use DBD::mysql;
+
+say "Drivers disponibles: " . Dumper( [ DBI->available_drivers ] );
+
+my $usuario = 'curso';
+my $password = 'curso'; # según el SMBD, podría utilizarse un archivo separado
+                        # para la contraseña y no necesitarla en el programa
+
+## ejemplo con PostgreSQL
+my $dbh_pg = DBI->connect("dbi:Pg:dbname=dvdrental;host=127.0.0.1", $usuario, $password, { pg_enable_utf8 => 1 });
+
+my $ar_pg = $dbh_pg->selectall_arrayref('SELECT film_id, title FROM film LIMIT 10');
+
+say "Primeras 10 películas en PostgreSQL: " . Dumper $ar_pg;
+
+$dbh_pg->disconnect;
+
+## ejemplo con MySQL
+
+my $dbh_mysql = DBI->connect("dbi:mysql:database=sakila;host=127.0.0.1", $usuario, $password, { mysql_enable_utf8 => 1 });
+
+my $ar_mysql = $dbh_mysql->selectall_arrayref('SELECT film_id, title FROM film LIMIT 10');
+
+say "Primeras 10 películas en MySQL: " . Dumper $ar_pg;
+
+$dbh_mysql->disconnect;
+
+say "Drivers instalados: " . Dumper( { DBI->installed_drivers } );
